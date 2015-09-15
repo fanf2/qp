adc2ae58d80df70456e9bb9d51d9c55953ca2d5c
Tony Finch <dot@dotat.at>
2015-09-15 18:39:26 +0100

    nibble-trie: better variable names in function prototypes

--- nibble-trie.h
+++ nibble-trie.h
@@ -6,17 +6,17 @@ typedef struct Tree Tree;
 // Get the value associated with a key.
 // Returns NULL if the key is not in the tree.
 //
-void *Tget(Tree *t, const char *key);
+void *Tget(Tree *tree, const char *key);
 
 // Associate a key with a value in a tree.
 // Value pointer must be word-aligned.
 // To delete a key, set its value to NULL.
 // Key and value are borrowed not copied.
 //
-Tree *Tset(Tree *t, const char *key, void *val);
+Tree *Tset(Tree *tree, const char *key, void *value);
 
 // Find the next key in the tree.
 // To find the first key, pass key = NULL.
 // Returns NULL when there are no more keys.
 //
-const char *Tnext(Tree *t, const char *key);
+const char *Tnext(Tree *tree, const char *key);
