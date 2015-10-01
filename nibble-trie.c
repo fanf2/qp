8660897c19d468aa4e66af6f0b11c0ee0cb222bb
Tony Finch <dot@dotat.at>
2015-10-01 20:31:16 +0100

    Use Tbl.h API

--- nibble-trie.c
+++ nibble-trie.c
@@ -1,4 +1,4 @@
-// A combination of DJB's crit-bit trees (http://cr.yp.to/critbit.html)
+// A combination of DJB's crit-bit tries (http://cr.yp.to/critbit.html)
 // and Phil Bagwell's (hashed) array-mapped tries
 // http://infoscience.epfl.ch/record/64394/files/triesearches.pdf
 // http://infoscience.epfl.ch/record/64398/files/idealhashtrees.pdf
@@ -12,9 +12,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "nibble-trie.h"
+#include "Tbl.h"
+#include "Tbl.c"
 
 typedef unsigned char byte;
+typedef unsigned int uint;
 
 // XXX Assume a little-endian struct layout, so that the flag bits
 // fall in the clear bits at the bottom of the pointer. This needs
@@ -35,7 +37,7 @@ typedef unsigned char byte;
 // word-aligned; the key pointer can be byte-aligned.
 
 typedef struct Tbranch {
-	union Tnode *twigs;
+	union Trie *twigs;
 	uint64_t
 		flags : 2,
 		index : 46,
@@ -47,19 +49,19 @@ typedef struct Tleaf {
 	void *val;
 } Tleaf;
 
-typedef union Tnode {
+typedef union Trie {
 	struct Tleaf   leaf;
 	struct Tbranch branch;
-} Tnode;
+} Trie;
 
-struct Tree {
-	union Tnode root;
+struct Tbl {
+	union Trie root;
 };
 
 // Test flags to determine type of this node.
 
 static inline bool
-isbranch(Tnode *t) {
+isbranch(Trie *t) {
 	return(t->branch.flags != 0);
 }
 
@@ -73,48 +75,52 @@ isbranch(Tnode *t) {
 // 1 -> 1 -> 4
 // 2 -> 0 -> 0
 
-static inline unsigned
-nibbit(byte k, unsigned flags) {
-	unsigned mask = ((flags - 2) ^ 0x0f) & 0xff;
-	unsigned shift = (2 - flags) << 2;
+static inline uint
+nibbit(byte k, uint flags) {
+	uint mask = ((flags - 2) ^ 0x0f) & 0xff;
+	uint shift = (2 - flags) << 2;
 	return(1 << ((k & mask) >> shift));
 }
 
-static inline unsigned
-twigbit(Tnode *t, const char *key, size_t len) {
+static inline uint
+twigbit(Trie *t, const char *key, size_t len) {
 	uint64_t i = t->branch.index;
 	if(i > len) return(0);
 	return(nibbit((byte)key[i], t->branch.flags));
 }
 
 static inline bool
-hastwig(Tnode *t, unsigned bit) {
+hastwig(Trie *t, uint bit) {
 	return(t->branch.bitmap & bit);
 }
 
-static inline int
-twigoff(Tnode *t, unsigned bit) {
-	return(__builtin_popcount(t->branch.bitmap & (bit - 1)));
+static inline uint
+popcount(uint word) {
+	return((uint)__builtin_popcount(word));
 }
 
-static inline int
-twigmax(Tnode *t) {
-	return(__builtin_popcount(t->branch.bitmap));
+static inline uint
+twigoff(Trie *t, uint bit) {
+	return(popcount(t->branch.bitmap & (bit - 1)));
 }
 
-static inline Tnode *
-twig(Tnode *t, int i) {
+static inline uint
+twigmax(Trie *t) {
+	return(popcount(t->branch.bitmap));
+}
+
+static inline Trie *
+twig(Trie *t, uint i) {
 	return(&t->branch.twigs[i]);
 }
 
 void *
-Tget(Tree *tree, const char *key) {
-	if(tree == NULL)
+Tgetl(Tbl *tbl, const char *key, size_t len) {
+	if(tbl == NULL)
 		return(NULL);
-	Tnode *t = &tree->root;
-	size_t len = strlen(key);
+	Trie *t = &tbl->root;
 	while(isbranch(t)) {
-		unsigned b = twigbit(t, key, len);
+		uint b = twigbit(t, key, len);
 		if(!hastwig(t, b))
 			return(NULL);
 		t = twig(t, twigoff(t, b));
@@ -125,51 +131,53 @@ Tget(Tree *tree, const char *key) {
 		return(NULL);
 }
 
-static const char *
-next_rec(Tnode *t, const char *key, size_t len) {
+static bool
+next_rec(Trie *t, const char **pkey, size_t *plen, void **pval) {
 	if(isbranch(t)) {
 		// This loop normally returns immediately, except when our
 		// key is the last in its twig, in which case the loop tries
 		// the next twig. Or if the key's twig is missing we run zero
 		// times.
-		unsigned b = twigbit(t, key, len);
-		for(int i = twigoff(t, b), j = twigmax(t); i < j; i++) {
-			const char *found = next_rec(twig(t, i), key, len);
-			if(found) return(found);
-		}
+		uint b = twigbit(t, *pkey, *plen);
+		for(uint i = twigoff(t, b), j = twigmax(t); i < j; i++)
+			if(next_rec(twig(t, i), pkey, plen, pval))
+				return(true);
+		return(false);
+	}
+	if(*pkey == NULL ||
+	   strcmp(*pkey, t->leaf.key) < 0) {
+		*pkey = t->leaf.key;
+		*plen = strlen(*pkey);
+		*pval = t->leaf.val;
+		return(true);
+	}
+	return(false);
+}
+
+bool
+Tnextl(Tbl *tbl, const char **pkey, size_t *plen, void **pval) {
+	if(tbl == NULL) {
+		*pkey = NULL;
+		*plen = 0;
 		return(NULL);
 	}
-	if(key == NULL ||
-	    strcmp(key, t->leaf.key) < 0)
-		return(t->leaf.key);
-	else
-		return(NULL);
+	return(next_rec(&tbl->root, pkey, plen, pval));
 }
 
-const char *
-Tnext(Tree *tree, const char *key) {
-	if(tree == NULL)
-		return(NULL);
-	Tnode *t = &tree->root;
-	size_t len = key != NULL ? strlen(key) : 0;
-	return(next_rec(t, key, len));
-}
-
-static Tree *
-Tdel(Tree *tree, const char *key) {
-	Tnode *t = &tree->root, *p = NULL;
-	size_t len = strlen(key);
-	unsigned b;
+Tbl *
+Tdell(Tbl *tbl, const char *key, size_t len) {
+	Trie *t = &tbl->root, *p = NULL;
+	uint b = 0;
 	while(isbranch(t)) {
 		b = twigbit(t, key, len);
 		if(!hastwig(t, b))
-			return(tree);
+			return(tbl);
 		p = t; t = twig(t, twigoff(t, b));
 	}
 	if(strcmp(key, t->leaf.key) != 0)
-		return(tree);
+		return(tbl);
 	if(p == NULL) {
-		free(tree);
+		free(tbl);
 		return(NULL);
 	}
 	if(twigmax(p) == 2) {
@@ -177,51 +185,50 @@ Tdel(Tree *tree, const char *key) {
 		t = p->branch.twigs;
 		*p = *twig(p, !twigoff(t, b));
 		free(t);
-		return(tree);
+		return(tbl);
 	}
-	int s = twigoff(t, b); // split
-	int m = twigmax(p);
-	Tnode *twigs = malloc(sizeof(Tnode) * (m - 1));
+	uint s = twigoff(t, b); // split
+	uint m = twigmax(p);
+	Trie *twigs = malloc(sizeof(Trie) * (m - 1));
 	if(twigs == NULL) return(NULL);
-	memcpy(twigs, p->branch.twigs, sizeof(Tnode) * s);
-	memcpy(twigs+s, p->branch.twigs+s+1, sizeof(Tnode) * (m - s - 1));
+	memcpy(twigs, p->branch.twigs, sizeof(Trie) * s);
+	memcpy(twigs+s, p->branch.twigs+s+1, sizeof(Trie) * (m - s - 1));
 	free(p->branch.twigs);
 	p->branch.twigs = twigs;
 	p->branch.bitmap &= ~b;
-	return(tree);
+	return(tbl);
 }
 
-Tree *
-Tset(Tree *tree, const char *key, void *val) {
+Tbl *
+Tsetl(Tbl *tbl, const char *key, size_t len, void *val) {
 	// Ensure flag bits are zero.
 	if(((uint64_t)val & 3) != 0) {
 		errno = EINVAL;
 		return(NULL);
 	}
 	if(val == NULL)
-		return(Tdel(tree, key));
-	// First leaf in an empty tree?
-	if(tree == NULL) {
-		tree = malloc(sizeof(*tree));
-		if(tree == NULL) return(NULL);
-		tree->root.leaf.key = key;
-		tree->root.leaf.val = val;
-		return(tree);
+		return(Tdel(tbl, key));
+	// First leaf in an empty tbl?
+	if(tbl == NULL) {
+		tbl = malloc(sizeof(*tbl));
+		if(tbl == NULL) return(NULL);
+		tbl->root.leaf.key = key;
+		tbl->root.leaf.val = val;
+		return(tbl);
 	}
-	Tnode *t = &tree->root;
-	size_t len = strlen(key);
-	// Find the most similar leaf node in the tree. We will compare
+	Trie *t = &tbl->root;
+	// Find the most similar leaf node in the trie. We will compare
 	// its key with our new key to find the first differing nibble,
 	// which can be at a lower index than the point at which we
 	// detect a difference.
 	while(isbranch(t)) {
-		unsigned b = twigbit(t, key, len);
+		uint b = twigbit(t, key, len);
 		// Even if our key is missing from this branch we need to
 		// keep iterating down to a leaf. It doesn't matter which
 		// twig we choose since the keys are all the same up to this
 		// index. Note that blindly using twigcount(t, n) can cause
 		// an out-of-bounds index if it equals twigcount(t, 16).
-		int i = hastwig(t, b) ? twigoff(t, b) : 0;
+		uint i = hastwig(t, b) ? twigoff(t, b) : 0;
 		t = twig(t, i);
 	}
 	// Do the keys differ, and if so, where?
@@ -231,16 +238,16 @@ Tset(Tree *tree, const char *key, void *val) {
 			goto newkey;
 	}
 	t->leaf.val = val;
-	return(tree);
+	return(tbl);
 newkey:; // We have the branch's index; what are its flags?
 	byte k1 = (byte)key[i], k2 = (byte)t->leaf.key[i];
-	unsigned f =  k1 ^ k2;
+	uint f =  k1 ^ k2;
 	f = (f & 0xf0) ? 1 : 2;
 	// Prepare the new leaf.
-	unsigned b1 = nibbit(k1, f);
-	Tnode t1 = { .leaf = { .key = key, .val = val } };
+	uint b1 = nibbit(k1, f);
+	Trie t1 = { .leaf = { .key = key, .val = val } };
 	// Find where to insert a branch or grow an existing branch.
-	t = &tree->root;
+	t = &tbl->root;
 	while(isbranch(t)) {
 		if(i == t->branch.index && f == t->branch.flags)
 			goto growbranch;
@@ -248,33 +255,33 @@ newkey:; // We have the branch's index; what are its flags?
 			goto newbranch;
 		if(i < t->branch.index)
 			goto newbranch;
-		unsigned b = twigbit(t, key, len);
+		uint b = twigbit(t, key, len);
 		assert(hastwig(t, b));
 		t = twig(t, twigoff(t, b));
 	}
 newbranch:;
-	Tnode *twigs = malloc(sizeof(Tnode) * 2);
+	Trie *twigs = malloc(sizeof(Trie) * 2);
 	if(twigs == NULL) return(NULL);
-	Tnode t2 = *t; // Save before overwriting.
-	unsigned b2 = nibbit(k2, f);
+	Trie t2 = *t; // Save before overwriting.
+	uint b2 = nibbit(k2, f);
 	t->branch.twigs = twigs;
 	t->branch.flags = f;
 	t->branch.index = i;
 	t->branch.bitmap = b1 | b2;
 	*twig(t, twigoff(t, b1)) = t1;
 	*twig(t, twigoff(t, b2)) = t2;
-	return(tree);
+	return(tbl);
 growbranch:;
 	assert(!hastwig(t, b1));
-	int s = twigoff(t, b1); // split
-	int m = twigmax(t);
-	twigs = malloc(sizeof(Tnode) * (m + 1));
+	uint s = twigoff(t, b1); // split
+	uint m = twigmax(t);
+	twigs = malloc(sizeof(Trie) * (m + 1));
 	if(twigs == NULL) return(NULL);
-	memcpy(twigs, t->branch.twigs, sizeof(Tnode) * s);
-	memcpy(twigs+s, &t1, sizeof(Tnode));
-	memcpy(twigs+s+1, t->branch.twigs+s, sizeof(Tnode) * (m - s));
+	memcpy(twigs, t->branch.twigs, sizeof(Trie) * s);
+	memcpy(twigs+s, &t1, sizeof(Trie));
+	memcpy(twigs+s+1, t->branch.twigs+s, sizeof(Trie) * (m - s));
 	free(t->branch.twigs);
 	t->branch.twigs = twigs;
 	t->branch.bitmap |= b1;
-	return(tree);
+	return(tbl);
 }
