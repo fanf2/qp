3c898aab8bf59b36f129615f70536f1fd273017d
Tony Finch <dot@dotat.at>
2015-10-02 17:17:16 +0100

    A script to generate test inputs

---
+++ Tbl-test-generate.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+if (@ARGV < 2) {
+	die <<EOF;
+usage: $0 <in> <out> <file>
+	Read the <file> and randomly pick <in> lines from it,
+	then emit <out> lines of input for Tbl-test.{c,pl}.
+EOF
+}
+
+my $i = shift;
+my $o = shift;
+
+my @p = qw( - + );
+my @i = <>;
+my @a;
+
+push @a, splice @i, (int rand @i), 1 while $i--;
+print $p[int rand 2], $a[int rand @a] while $o--;
+
