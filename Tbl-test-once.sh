2c16fdc242c9f888c99850558c16912023c63c86
Tony Finch <dot@dotat.at>
2015-10-02 17:23:12 +0100

    A one-shot test triver

---
+++ Tbl-test-once.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+./Tbl-test-generate.pl "$@" >Tbl-test-input
+./Tbl-test.pl <Tbl-test-input >Tbl-test-out-pl
+./Tbl-test <Tbl-test-input >Tbl-test-out-c
+cmp Tbl-test-out-pl Tbl-test-out-c
+rm -f Tbl-test-input Tbl-test-out-pl Tbl-test-out-c
