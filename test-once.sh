34f328d19ced3239914fb417a0f3f1d57e08dd9d
Tony Finch <dot@dotat.at>
2015-10-16 16:01:41 +0100

    Test programs specified by Makefile

--- test-once.sh
+++ test-once.sh
@@ -2,20 +2,15 @@
 set -e
 if [ ! -f test-in ]
 then	printf 1>&2 "generating..."
-	./test-gen.pl "$@" >test-in
+	./test-gen.pl $1 $2 $3 >test-in
 	printf 1>&2 "done\n"
 fi
+shift 3
 time ./test.pl <test-in >test-out-pl
-time ./test-cb <test-in >test-out-cb
-time ./test-qp <test-in >test-out-qp
-time ./test-qn <test-in >test-out-qn
-time ./test-qs <test-in >test-out-qs
-time ./test-wp <test-in >test-out-wp
-#time ./test-ht <test-in | sort >test-out-ht
-cmp test-out-pl test-out-cb
-cmp test-out-pl test-out-qp
-cmp test-out-pl test-out-qn
-cmp test-out-pl test-out-qs
-cmp test-out-pl test-out-wp
-#cmp test-out-pl test-out-ht
+for i in "$@"
+do time ./test-$i <test-in >test-out-$i
+done
+for i in "$@"
+do cmp test-out-pl test-out-$i
+done
 rm -f test-in test-out-??
