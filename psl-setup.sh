fc08d711e7aca19f28e1d1fb86781b0d88b90fff
Tony Finch <dot@dotat.at>
2016-11-23 23:52:35 +0000

    strip unicode names on machines without idn

--- psl-setup.sh
+++ psl-setup.sh
@@ -3,7 +3,12 @@
 [ -f public_suffix_list.dat ] ||
 	curl -fO https://publicsuffix.org/list/public_suffix_list.dat
 
+if ! idn=$(type -p idn)
+then idn=cat
+fi
+
 cat public_suffix_list.dat |
-	sed 's| *//.*||;s|^\*\.||;/^!/d;/^$/d' |
-	idn |
+	sed 's| *//.*||;s|^\*\.||' |
+	$idn |
+	egrep '^[a-z0-9.-]+$' |
 	env -i LANG=C sort > psl.dat
