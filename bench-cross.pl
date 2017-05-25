83c3b34d0f504656d5d9638ff28699e91918c693
Tony Finch <dot@dotat.at>
2017-05-25 13:39:53 +0100

    A cross between bench-multi and bench-more, with colours

---
+++ bench-cross.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use MIME::Base64;
+
+sub maxlen {
+	return (sort { $a <=> $b } map { length } @_)[-1];
+}
+
+sub usage {
+	die <<EOF;
+usage: $0 <count> <prog>... -- <input>...
+EOF
+}
+
+usage if @ARGV < 4 or $ARGV[0] !~ m{^\d+$};
+my $count = shift;
+
+my @prog;
+
+push @prog, shift while @ARGV and $ARGV[0] ne '--';
+usage if '--' ne shift @ARGV;
+
+my %col;
+for my $i (0 .. $#prog) {
+	$col{$prog[$i]} = sprintf "\e[3%dm", 1 + $i %7;
+}
+
+my @file = @ARGV;
+
+my $wp = maxlen @prog;
+my $wf = maxlen @file, "0.000";
+my $waf = ($wf+1) * scalar @file;
+
+my %stats;
+
+open my $rnd, '<', '/dev/urandom'
+    or die "open /dev/urandom: $!\n";
+
+for (my $N = 1 ;; ++$N) {
+	my $seed;
+	sysread $rnd, $seed, 12;
+	$seed = encode_base64 $seed, "";
+
+	for my $file (@file) {
+		for my $prog (@prog) {
+			print "$prog $seed $count $file\n";
+			for (qx{$prog $seed $count $file}) {
+				if(m{^(\w+)... ([0-9.]+) s$}) {
+					my $test = $1;
+					my $time = $2;
+					$stats{$test}{$prog}{$file}{this} = $time;
+					$stats{$test}{$prog}{$file}{min} = $time
+					    if not defined $stats{$test}{$prog}{$file}{min}
+						or $stats{$test}{$prog}{$file}{min} > $time;
+					$stats{$test}{$prog}{$file}{tot} += $time;
+					$stats{$test}{$prog}{$file}{tot2} += $time * $time;
+				}
+			}
+		}
+	}
+
+	printf "%-*s ", $wp + $wf, "round $N";
+	printf " | %-31s", $_ for sort keys %stats;
+	print "\n";
+	for my $file (@file) {
+		for my $prog (@prog) {
+			printf "%s%-*s %-*s", $col{$prog}, $wp, $prog, $wf, $file;
+			for my $test (sort keys %stats) {
+				my $mean = $stats{$test}{$prog}{$file}{tot} / $N;
+				my $var = $stats{$test}{$prog}{$file}{tot2} / $N - $mean * $mean;
+				printf " | \e[2m(%.3f)\e[22m %.3f < %.3f +/- %.3f",
+				    $stats{$test}{$prog}{$file}{this},
+				    $stats{$test}{$prog}{$file}{min},
+				    $mean, $var ** 0.5;
+
+			}
+			print "\e[0m\n";
+		}
+	}
+}
